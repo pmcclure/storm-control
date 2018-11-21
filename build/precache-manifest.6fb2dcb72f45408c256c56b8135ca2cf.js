09d85a3e8bfc17c9ba8b4da257ea3bcb940cbaad
Paul McClure <paul.m.mcclure@gmail.com>
2018-11-21 14:56:04 -0800

    update build

--- build/precache-manifest.6fb2dcb72f45408c256c56b8135ca2cf.js
+++ build/precache-manifest.6fb2dcb72f45408c256c56b8135ca2cf.js
@@ -4,43 +4,43 @@ self.__precacheManifest = [
     "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
   },
   {
-    "revision": "337d8d2bbc026cfeee20",
-    "url": "/static/css/main.f0f0d6e3.chunk.css"
+    "revision": "7b841d38d92d4a7c7b8c",
+    "url": "/static/css/main.c00ff6b7.chunk.css"
   },
   {
     "revision": "89889688147bd7575d6327160d64e760",
     "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
   },
   {
-    "revision": "22ed1d973810f2f32e0c",
-    "url": "/static/js/1.22ed1d97.chunk.js"
+    "revision": "2de8bcb7f782aa9bf0bd",
+    "url": "/static/js/1.2de8bcb7.chunk.js"
   },
   {
     "revision": "229c360febb4351a89df",
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "337d8d2bbc026cfeee20",
-    "url": "/static/js/main.337d8d2b.chunk.js"
-  },
-  {
-    "revision": "fa2772327f55d8198301fdb8bcfc8158",
-    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+    "revision": "7b841d38d92d4a7c7b8c",
+    "url": "/static/js/main.7b841d38.chunk.js"
   },
   {
     "revision": "448c34a56d699c29117adc64c43affeb",
     "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
   },
   {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
     "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
     "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
   },
   {
-    "revision": "22ed1d973810f2f32e0c",
-    "url": "/static/css/1.c21fa068.chunk.css"
+    "revision": "2de8bcb7f782aa9bf0bd",
+    "url": "/static/css/1.dba6440b.chunk.css"
   },
   {
-    "revision": "af24359d0162045920a3f0c0c1a9d3f1",
+    "revision": "02d496e8c1f199a82c824547e0523deb",
     "url": "/index.html"
   }
 ];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.985676fc042b53ec645fa779073ed6e9.js"
+  "/precache-manifest.6fb2dcb72f45408c256c56b8135ca2cf.js"
 );
 
 workbox.clientsClaim();
